0ca7bae72ddaa3f520d6577e349a94db4c3ae051
GameduJS <yves.n.becker@gmail.com>
2023-07-20 22:21:16 +0200

    Adjustment for issue #13

--- src/main/java/de/gamedude/easyvillagertrade/utils/TradingState.java
+++ src/main/java/de/gamedude/easyvillagertrade/utils/TradingState.java
@@ -2,6 +2,7 @@ package de.gamedude.easyvillagertrade.utils;
 
 public enum TradingState {
     CHECK_OFFERS(),
+    WAIT_JOB_LOSS(),
     WAIT_PROFESSION(),
     BREAK_WORKSTATION(),
     PLACE_WORKSTATION(),
