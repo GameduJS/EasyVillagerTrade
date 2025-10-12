4a221a603485414dfbee062729d9dbf5fcb6e7b1
GameduJS <yves.n.becker@gmail.com>
2025-10-12 22:35:47 +0200

    fixes game crash

--- src/main/java/de/gamedude/easyvillagertrade/screen/widget/TradeRequestListWidget.java
+++ src/main/java/de/gamedude/easyvillagertrade/screen/widget/TradeRequestListWidget.java
@@ -117,8 +117,10 @@ public class TradeRequestListWidget extends AbstractParentElement implements Dra
 
         // #HACK, since the "scrolled" entries "stack on top of each other" => the first would be removed each time
         int entriesSkipped = Math.ceilDiv( (int) scrollAmount, ENTRY_HEIGHT + 5 );
-        if ( children.indexOf(tradeRequestEntry) == 0 && children.size() > ENTRIES_PER_PAGE )
+        if ( children.indexOf(tradeRequestEntry) == 0 && children.size() > ENTRIES_PER_PAGE ) {
+            scrollAmount-=(ENTRY_HEIGHT + 5);
             tradeRequestEntry = children.get(entriesSkipped);
+        }
 
         children.remove(tradeRequestEntry);
         modBase.getTradeRequestContainer().removeTradeRequest(tradeRequestEntry.tradeRequest);
